0fc9a60025c556cafe29147d1a8b4e9b2a15bde2
Saif uddin Babar <mdbabar4202@gmail.com>
2025-08-28 19:10:07 +0600

    add last commit

--- home.js
+++ home.js
@@ -3,6 +3,24 @@ function getElement(id)
    return document.getElementById(id)
 }
 
+
+
+// btn hover effect 
+const btns = document.querySelectorAll(".btn2");
+
+for (const btn of btns) {
+
+  btn.addEventListener("mouseenter", function () {
+    btn.style.backgroundColor = "gray"; 
+    btn.style.transition = "background-color 0.3s ease"; 
+  });
+
+  btn.addEventListener("mouseleave", function () {
+    btn.style.backgroundColor = "#16a34a";
+  });
+}
+
+
 // call btn 
 document.getElementById('cardbox').addEventListener("click", function(e) {
     if (e.target.classList.contains("btn2")) {
@@ -26,17 +44,17 @@ if (totalcoin< 0) {
        
 
 
-        alert(title+contactNUmber);
+        alert(title+ " " +contactNUmber);
       
       maincoin.innerText = totalcoin; 
       const container = getElement('containerforhistory')
 
     const newdiv = document.createElement('div')
 newdiv.innerHTML = `
-<div class="flex justify-between">
+<div class="flex justify-between border border-gray-200 rounded-xl p-5">
         <div>
           <h1 class="font-bold">${title}</h1>
-          <p class="text-gray-400">${contactNUmber}</p>
+          <p class="font-bold text-black-800">${contactNUmber}</p>
         </div>
         <div>
          <p>${new Date().toLocaleTimeString()}</p>
@@ -52,14 +70,15 @@ container.appendChild(newdiv)
     }
 });
 
+// clearbtn
 getElement("clearbtn").addEventListener('click',function()
 {
  const container = getElement('containerforhistory')
 container.innerHTML = ""
 
 })
-
-// clearbtn
+ 
+// copy button
 document.getElementById('cardbox').addEventListener("click", function(e) {
     if (e.target.classList.contains("btn1")) {
         
@@ -81,4 +100,6 @@ document.getElementById('cardbox').addEventListener("click", function(e) {
         heartElement.innerText = totalheart;
     }
 });
+
+
 
